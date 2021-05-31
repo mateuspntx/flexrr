825eed8e6c942407de157f008cd62bbe82565c86
mateuspntx <mateuspntx@gmail.com>
2021-05-31 03:23:52 -0300

    chore: create search section style

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,21 +1,25 @@
 import { Link } from 'react-router-dom';
 
-import { Container, Logo, MenuWrapper } from './styles';
+import * as S from './styles';
 
 const Navbar = () => {
   return (
-    <Container>
-      <Logo>
+    <S.Container>
+      <S.Logo>
         <Link to="/">
           <h1>Flexrr</h1>
         </Link>
-      </Logo>
-      <MenuWrapper>
+      </S.Logo>
+      <S.MenuWrapper>
+        <S.SearchWrapper>
+          <S.SearchButton />
+          <S.SearchInput placeholder="Movies, TV Shows, People..." />
+        </S.SearchWrapper>
         <Link to="/">Discover</Link>
         <Link to="/movies">Movies</Link>
         <Link to="/tv">TV Shows</Link>
-      </MenuWrapper>
-    </Container>
+      </S.MenuWrapper>
+    </S.Container>
   );
 };
 
