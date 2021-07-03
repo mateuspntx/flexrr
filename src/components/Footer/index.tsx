b5a08fd1e991b0876ace74e12638b860c23b07ed
mateuspntx <mateuspntx@gmail.com>
2021-07-03 01:13:20 -0300

    chore(comp): add credits in Footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,9 +1,27 @@
 import * as S from './styles';
 
+import TMDbLogo from '../../assets/images/tmdb_logo.svg';
+
 const Footer = () => {
   return (
     <S.Container>
-      <p>You've reached the end :)</p>
+      <p>
+        Made with <span className="heart">&hearts; </span> by{' '}
+        <a
+          href="https://github.com/mateuspntx"
+          target="_blank"
+          rel="​noopener noreferrer"
+        >
+          Mateus Pontes
+        </a>
+      </p>
+
+      <p>
+        Powered by{' '}
+        <a href="https://www.themoviedb.org/" target="_blank" rel="​noopener noreferrer">
+          <img src={TMDbLogo} alt="TMDb" width="50px" />
+        </a>
+      </p>
     </S.Container>
   );
 };
--- src/components/Footer/styles.ts
+++ src/components/Footer/styles.ts
@@ -1,11 +1,19 @@
 import styled from 'styled-components';
 
 export const Container = styled.footer`
+  padding: 0 5rem;
   display: flex;
-  justify-content: center;
+  justify-content: space-around;
   align-items: center;
   align-content: center;
+  flex-wrap: wrap;
+  gap: 10px;
   width: 100%;
   height: 100px;
   background-color: ${({ theme }) => theme.colors.backgroundSecondary};
+
+  .heart {
+    color: red;
+    font-size: 20px;
+  }
 `;
