f5347d7efe03d94ffede4ac222b6b6cbc598d086
mateuspntx <mateuspntx@gmail.com>
2021-07-25 17:08:46 -0300

    chore(hook): update useDocumentTitle

--- src/hooks/useDocumentTitle.ts
+++ src/hooks/useDocumentTitle.ts
@@ -1,9 +1,32 @@
-import { useEffect } from 'react';
+// hook from https://github.com/streamich/react-use/blob/master/src/useTitle.ts
 
-const useDocumentTitle = (title: string) => {
-  useEffect(() => {
-    window.document.title = title;
-  }, [title]);
+import { useEffect, useRef } from 'react';
+
+export interface useDocumentTitleOptions {
+  restoreOnUnmount?: boolean;
+}
+
+const DEFAULT_USE_TITLE_OPTIONS: useDocumentTitleOptions = {
+  restoreOnUnmount: false,
 };
 
-export default useDocumentTitle;
+function useDocumentTitle(
+  title: string,
+  options: useDocumentTitleOptions = DEFAULT_USE_TITLE_OPTIONS
+) {
+  const prevTitleRef = useRef(document.title);
+  document.title = title;
+  useEffect(() => {
+    if (options && options.restoreOnUnmount) {
+      return () => {
+        document.title = prevTitleRef.current;
+      };
+    } else {
+      return;
+    }
+  }, []);
+}
+
+export default typeof document !== 'undefined'
+  ? useDocumentTitle
+  : (_title: string) => {};
