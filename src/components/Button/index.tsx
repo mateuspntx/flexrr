91b73f2a04576465c96f860df237ad64242ffa4b
mateuspntx <mateuspntx@gmail.com>
2021-08-04 18:42:25 -0300

    chore(Button): create variants

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -3,11 +3,16 @@ import { ButtonHTMLAttributes } from 'react';
 import * as S from './styles';
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  variant?: 'transparent' | 'red' | 'orange';
   children: React.ReactNode;
 }
 
-const Button = ({ children, ...rest }: ButtonProps) => {
-  return <S.Button {...rest}>{children}</S.Button>;
+const Button = ({ variant, children, ...rest }: ButtonProps) => {
+  return (
+    <S.Button {...rest} variant={variant}>
+      {children}
+    </S.Button>
+  );
 };
 
 export default Button;
